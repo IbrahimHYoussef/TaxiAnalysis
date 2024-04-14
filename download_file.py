8088406526606f74481b11bdc3ef0ec9d35142d4
ibrahim <ibrahimhyoussef18@gmail.com>
2024-04-14 14:45:57 +0200

    first commit

---
+++ download_file.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import os 
+
+# Make data dir if not exists
+def make_data_dir():
+    if "data" not in os.listdir():
+        os.mkdir("data")
+make_data_dir()
+
+def start_webdriver():
+    pass
+
+
+def end_webdriver():
+    pass
+
+
+
+
+
+
